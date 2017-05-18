f5b82117ea0414a29d4ef5601228b3b9aabd03bb
JulianDomingo <julian.domingo@utexas.edu>
2017-05-17 22:01:30 -0500

    Finished user show generator, added sample training set.

--- get_training_set.py
+++ get_training_set.py
@@ -1,44 +1,72 @@
-# Julian Domingo
-
 # Temporary script to gather 1,000,000 MAL users to use as a
 # training set for testing purposes.
 
-import re
-import requests
 from lxml.html import fromstring
 from xml.etree import ElementTree
-
-element_trees = dict()
-# TODO: Fix second regex.
-regex = {"\n": "", "'s.*": ""}
-
-def retrieve_usernames(number_of_requests):
-    users = 0
-
-    while users < number_of_requests:
-        response = requests.get("https://myanimelist.net/comments.php?id="
-                                + str(request))
-        
-        if response.status_code == 404:
-            continue
-
-        raw_user = fromstring(response.content).findtext(".//title")
-
-        # Does both regex replacements in a single pass.
-        regex = dict((re.escape(k), v) for k, v in list(regex.items()))
-        pattern = re.compile("|".join(regex.keys()))
-        username = pattern.sub(lambda x: regex[re.escape(x.group(0))], raw_user) 
-
-        element_trees.setdefault(text, None)    
-        users += 1
+import re
+import requests
 
 
-def retrive_element_trees():
-    for user in element_trees.keys():
-        response = requests.get("https://myanimelist.net/malappinfo.php?u=" + user) 
-        element_trees[user] = ElementTree.fromstring(response.content) 
+class DataGenerator(object):
+    def __init__(self):
+        self.element_trees = dict()
+        self.user_agent_header = {"User-Agent": "Data Scraper Script by user Almiria"} 
+
+    def retrieve_usernames(self, number_of_requests):
+        user = 1
+        counter = 0
+
+        while counter < number_of_requests:
+            response = requests.get("https://myanimelist.net/comments.php?id="
+                                    + str(user), headers=self.user_agent_header)
+            user += 1
+          
+            print(response.status_code)
+            if response.status_code == 404:
+                continue
+
+            raw_title = fromstring(response.content).findtext(".//title")
+            username = re.sub("'s.*$|\n", "", raw_title)
+            
+            self.element_trees.setdefault(username, [None]) 
+            
+            counter += 1
 
 
-retrieve_usernames(1000000)
-retrieve_element_trees()
+    def retrieve_element_trees(self):
+        for user in self.element_trees.keys():
+            response = requests.get("https://myanimelist.net/malappinfo.php?u="
+                                    + user, headers=self.user_agent_header) 
+            self.element_trees[user][0] = ElementTree.fromstring(response.content) 
+             
 
+    def extract_animes(self):
+        for user_info in self.element_trees.values():
+            self.record_rated_shows(user_info)
+
+
+    def record_rated_shows(self, user_info):
+        tree = user_info[0]
+
+        for anime in tree.findall("anime"):
+            if int(anime.find("my_watched_episodes").text) > 0:
+                user_info.append(anime.find("series_title").text)
+
+
+    def write_data(self):
+        data = "\n".join(", ".join(shows[1:]) for shows in self.element_trees.values()) 
+        training_set = open("training_set.txt", "w")
+        training_set.write(data)
+        training_set.close()
+
+
+def main():
+    generator = DataGenerator()
+    generator.retrieve_usernames(10)
+    generator.retrieve_element_trees()
+    generator.extract_animes()
+    generator.write_data()
+
+
+if __name__ == "__main__":
+    main()
